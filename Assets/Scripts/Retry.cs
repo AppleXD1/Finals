5180e31d5608a0334d2332e2afba7842ba559328
AppleXD1 <143732226+AppleXD1@users.noreply.github.com>
2026-04-28 00:19:56 -0400

    ere

--- Assets/Scripts/Retry.cs
+++ Assets/Scripts/Retry.cs
@@ -1,16 +1,33 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Retry : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Player player;
+    public Button Button;
+
+    private void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (player.isDead)
+        {
+            Button.enabled = true;
+        }
+        else
+        {
+            Button.enabled = false;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void ReloadScene()
+    {
+        // Loads the active scene by its string name
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
